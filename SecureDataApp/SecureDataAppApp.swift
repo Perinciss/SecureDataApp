b437db4707c64ce5f2cf495fc676276001c958bf
Kevin Heryanto <heryantokevin@gmail.com>
2023-09-07 02:25:41 +0700

    Initial Commit

---
+++ SecureDataApp/SecureDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureDataAppApp.swift
+//  SecureDataApp
+//
+//  Created by Kevin Heryanto on 07/09/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SecureDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
